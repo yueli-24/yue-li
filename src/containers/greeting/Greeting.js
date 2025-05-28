b15a79f92fc1c4b3500186bb180b58da376e9247
yueli-24 <yue.career2023@gmail.com>
2025-05-28 16:49:29 +0900

    feat: 更新首页动画 - 替换为新的 GIF 动画 - 优化布局和样式

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -2,11 +2,9 @@ import React, {useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
-import landingPerson from "../../assets/lottie/landingPerson";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import {illustration, greeting} from "../../portfolio";
+import {greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Greeting() {
@@ -53,14 +51,11 @@ export default function Greeting() {
             </div>
           </div>
           <div className="greeting-image-div">
-            {illustration.animated ? (
-              <DisplayLottie animationData={landingPerson} />
-            ) : (
-              <img
-                alt="man sitting on table"
-                src={require("../../assets/images/manOnTable.svg")}
-              ></img>
-            )}
+            <img
+              alt="greeting animation"
+              src={require("../../assets/lottie/GIF_20250528_164517.gif")}
+              className="greeting-image"
+            />
           </div>
         </div>
       </div>
